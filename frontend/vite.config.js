389f3330470f8b555e7273c4f9bd52f2b685f991
Aniket Tiwari <pt5602238@gmail.com>
2026-05-20 03:47:50 +0530

    frontend deploy changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://socialmedia-yd54.onrender.com",
       },
     },
   },
